439f6ecd0c14801a43a6e666381490d4ac1beb5c
Eden Brekke <eden.lorrai@gmail.com>
2022-12-21 14:07:10 -0800

    added banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+// Typography is like a p tag in material UI
+import { Box, Stack, Typography } from '@mui/material';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Typography color='#FF2625' fontWeight='600' fontSize='26px'>
+      Fitness Club
+    </Typography>
   )
 }
 
