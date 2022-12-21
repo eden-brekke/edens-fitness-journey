f2a2f856e9038d6c83fe433ec402392ddff697f5
Eden Brekke <eden.lorrai@gmail.com>
2022-12-21 14:10:47 -0800

    added style to box around typography

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -4,9 +4,22 @@ import { Box, Stack, Typography } from '@mui/material';
 
 const HeroBanner = () => {
   return (
-    <Typography color='#FF2625' fontWeight='600' fontSize='26px'>
-      Fitness Club
-    </Typography>
+    <Box
+      sx={{
+        mt: {
+          lg: '212px',
+          xs: '70px'
+        },
+        ml: {
+          sm: '50px'
+        }
+      }}
+      position='relative'
+      p='20px'>
+      <Typography color='#FF2625' fontWeight='600' fontSize='26px'>
+        Fitness Club
+      </Typography>
+    </Box>
   )
 }
 
