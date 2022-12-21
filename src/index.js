3d4476e41e89f5106384a2790b8fad7e1449f47b
Eden Brekke <eden.lorrai@gmail.com>
2022-12-21 11:00:38 -0800

    imported browserrouter and wrapped app within it so that we can use the routes we made on app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 
 
 root.render(
-  <App />
+  // we need to wrap out App in the browser router so that we can use the routes we made
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
