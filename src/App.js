ca3ffdb1117ce8e4ffb25dfd09c110579899f67a
Eden Brekke <eden.lorrai@gmail.com>
2022-12-23 12:06:43 -0800

    fixed formatting

--- src/App.js
+++ src/App.js
@@ -19,13 +19,27 @@ const App = () => {
   return (
     // This box will contain the Navbar and the Routes between pages
     // material ui lets us use the box, set the width then set a responsive width and a margin inline
-    <Box width='400px' sx={{ width: { xl: '1488px'}}} m='auto'>
-      <Navbar /> 
+    <Box
+      width='400px'
+      sx={{
+        width: {
+          xl: '1488px'
+        }
+      }}
+      m='auto'
+    >
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/exercise/:id' element={<ExerciseDetail />}/>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/exercise/:id'
+          element={<ExerciseDetail />}
+        />
       </Routes>
-      <Footer/>
+      <Footer />
     </Box>
   )
 }
