0c64ff2fdf057fa79ddbda5efbc41cc4f0b3a561
Eden Brekke <eden.lorrai@gmail.com>
2022-12-27 14:07:19 -0800

    implemented pagination from mui

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -6,6 +6,17 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, bodyPart, setExercises }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = function(e, value){
+    setCurrentPage(value);
+    window.scrollTo({top:1800, behavior:'smooth'})
+  }
   return (
     <Box
       id='exercises'
@@ -34,12 +45,28 @@ const Exercises = ({ exercises, bodyPart, setExercises }) => {
         flexWrap='wrap'
         justifyContent='center'
       >
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
           <ExerciseCard
             key={index}
             exercise={exercise}
           />
         ))}
+      </Stack>
+      <Stack
+        mt='100px'
+        alignItems='center'
+      >
+        {exercises.length > exercisesPerPage && (
+          <Pagination 
+            color='standard'
+            shape='rounded'
+            defaultPage={1}
+            count={Math.ceil(exercises.length/exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size='large'
+          />
+        )}
       </Stack>
     </Box>
   )
