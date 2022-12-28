4ee606979fa16003522151837ea71ea014077cc3
Eden Brekke <eden.lorrai@gmail.com>
2022-12-28 11:23:54 -0800

    deconstructed object and rendered gif

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Typography, Stack, Button } from '@mui/material';
 
-const Detail = () => {
+import BodyPartImage from '../assets/icons/body-part.png';
+import TargetImage from '../assets/icons/target.png';
+import EquipmentImage from '../assets/icons/equipment.png';
+
+const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail; // object destructuring, allowing us to just call bodyPart instead of exerciseDetail.bodyPart etc
+
   return (
-    <div>Detail</div>
+    <Stack
+      gap='60px'
+      sx={{
+        flexDirection: {
+          lg: 'row'
+        },
+        p: '20px',
+        alignItems: 'center'
+      }}
+    >
+      <img
+        src={gifUrl}
+        alt={name}
+        loading='lazy'
+        className='detail-image'
+      />
+    </Stack>
   )
 }
 
