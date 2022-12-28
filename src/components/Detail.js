3eb9fe6f53caf9613707300dfa5315f3ab822e92
Eden Brekke <eden.lorrai@gmail.com>
2022-12-28 11:48:53 -0800

    finished detail

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -8,6 +8,21 @@ import EquipmentImage from '../assets/icons/equipment.png';
 const Detail = ({ exerciseDetail }) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail; // object destructuring, allowing us to just call bodyPart instead of exerciseDetail.bodyPart etc
 
+  const extraDetail = [
+    {
+      icon: BodyPartImage,
+      name: bodyPart,
+    },
+    {
+      icon: TargetImage,
+      name: target,
+    },
+    {
+      icon: EquipmentImage,
+      name: equipment,
+    },
+  ]
+
   return (
     <Stack
       gap='60px'
@@ -25,6 +40,53 @@ const Detail = ({ exerciseDetail }) => {
         loading='lazy'
         className='detail-image'
       />
+      <Stack
+        sx={{
+          gap: {
+            lg: '35px', 
+            xs: '20px'
+          }
+        }}
+      >
+        <Typography variant='h3'>
+          {name}
+        </Typography>
+        <Typography variant='h6'>
+          The {name} {` `} exercise is a great way to target your {target}. Exercising is proven to improve your mood, sleep and give you more energy throughout your day.
+        </Typography>
+        {extraDetail.map((item) => (
+          <Stack 
+            key={item.name}
+            direction='row'
+            gap='24px'
+            alignItems='center'
+          >
+            <Button
+              sx= {{
+                background: '$fff2db',
+                borderRadius: '50%',
+                width: '100px',
+                height: '100px',
+              }}
+            >
+              <img 
+                src={item.icon} 
+                alt={bodyPart}
+                style={{
+                  width: '50px',
+                  height: '50px',
+                }}
+              />
+            </Button>
+            <Typography 
+              variant='h5'
+              textTransform='capitalize'
+            >
+              {item.name}
+            </Typography>
+          </Stack>
+        ))}
+      </Stack>
     </Stack>
   )
 }
