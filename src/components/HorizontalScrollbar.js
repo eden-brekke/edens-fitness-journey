4dcb8c45e2a4705c78d6ebde8a3b29dc6e4874da
Eden Brekke <eden.lorrai@gmail.com>
2022-12-29 13:28:33 -0800

    adjust to either render icons or exercises depending on which prop is passed in

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react'
 import { Box, Typography } from '@mui/material'
-import { ScrollMenu, VisibilityContext} from 'react-horizontal-scrolling-menu';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
-import BodyPart from './BodyPart'
+import BodyPart from './BodyPart';
+import ExerciseCard from './ExerciseCard';
 import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
@@ -26,7 +27,7 @@ const RightArrow = () => {
   );
 };
 
-const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyParts }) => {
   return (
     <ScrollMenu
       LeftArrow={LeftArrow}
@@ -38,12 +39,15 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
           itemId={item.id || item}
           title={item.id || item}
           m='0, 40px'
-        > 
-        <BodyPart 
-          item={item} 
-          bodyPart={bodyPart} 
-          setBodyPart={setBodyPart}
-        />
+        >
+          {isBodyParts
+            ? <BodyPart
+              item={item}
+              bodyPart={bodyPart}
+              setBodyPart={setBodyPart}
+            />
+            : <ExerciseCard exercise={item} />
+          }
         </Box>
       )
       )
